f3f0a30cf47e65db4821c18f813707b384f01583
neilmerin <merin.serdeeneil.nakpil@gmail.com>
2021-12-01 11:32:48 +0800

    added variable called result and its out value in script.js

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ function compute() {
   var years = document.getElementById("years").value;
   var year = new Date().getFullYear() + parseInt(years);
   var interest = (principal * years * rate) / 100;
+
+  console.log(interest, year, years);
+
+  document.getElementById("result").innerHTML =
+    "If you deposit " +
+    principal +
+    ",<br>at an interest rate of " +
+    rate +
+    "%<br>You will receive an amount of " +
+    interest +
+    ",<br>in the year " +
+    year +
+    "<br>";
 }
 
 function updateRate() {
   var rateval = document.getElementById("rate").value;
-  document.getElementById("rate_val").innerText = rateval;
+  document.getElementById("rate_val").innerText = `${rateval} %`;
 }
