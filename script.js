8bf8b668c625c0fdf3199a6fa102f1d9d8ecd27e
neilmerin <merin.serdeeneil.nakpil@gmail.com>
2021-12-01 12:47:11 +0800

    added validation for principal if the user enters zero or negative values in script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function compute() {
-  var principal = document.getElementById("principal").value;
+  let principal = document.getElementById("principal");
+  if (principal.value <= 0) {
+    alert("Enter a positive number");
+    principal.focus();
+  }
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
   var year = new Date().getFullYear() + parseInt(years);
-  var interest = (principal * years * rate) / 100;
+  var interest = (principal.value * years * rate) / 100;
 
   document.getElementById(
     "result"
-  ).innerHTML = `If you deposit <b>${principal}</b> at an interest rate of <b>${rate} %.</b>
+  ).innerHTML = `If you deposit <b>${principal.value}</b> at an interest rate of <b>${rate} %.</b>
   You will receive an amount of <b>${interest}</b> in the year <b>${year}</b> <br>`;
 }
 
