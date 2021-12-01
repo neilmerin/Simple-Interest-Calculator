9175faa524c92057455e9903d247e44f871fc61b
neilmerin <merin.serdeeneil.nakpil@gmail.com>
2021-12-01 11:02:02 +0800

    created a variable called years and assigned to it the value of the input element years in script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function compute() {
   var principal = document.getElementById("principal").value;
   var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
 }
