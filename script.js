488133f103d2f14814f172dc37b18e910952307d
neilmerin <merin.serdeeneil.nakpil@gmail.com>
2021-12-01 11:41:21 +0800

    highligted the output values of result in script.js

--- script.js
+++ script.js
@@ -5,16 +5,10 @@ function compute() {
   var year = new Date().getFullYear() + parseInt(years);
   var interest = (principal * years * rate) / 100;
 
-  document.getElementById("result").innerHTML =
-    "If you deposit " +
-    principal +
-    ",<br>at an interest rate of " +
-    rate +
-    "%<br>You will receive an amount of " +
-    interest +
-    ",<br>in the year " +
-    year +
-    "<br>";
+  document.getElementById(
+    "result"
+  ).innerHTML = `If you deposit <b>${principal}</b> at an interest rate of <b>${rate} %.</b>
+  You will receive an amount of <b>${interest}</b> in the year <b>${year}</b> <br>`;
 }
 
 function updateRate() {
