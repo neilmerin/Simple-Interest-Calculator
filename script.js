dbb76462cbaa40d2565e777b8bf7cbd90d35bbf4
neilmerin <merin.serdeeneil.nakpil@gmail.com>
2021-12-01 15:31:00 +0800

    updated the range slider style in style.css

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ function compute() {
 }
 
 function updateRate() {
-  var rateval = document.getElementById("rate").value;
-  document.getElementById("rate_val").innerText = `${rateval} %`;
+  var rateval = document.getElementById("rate");
+  document.getElementById("rate_val").innerText = `${rateval.value} %`;
 }
