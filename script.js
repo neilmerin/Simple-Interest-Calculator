7ce3a2ae9bbfbe0c3855a9fdc30ef38cea75c25e
neilmerin <merin.serdeeneil.nakpil@gmail.com>
2021-12-01 11:10:17 +0800

    created a logic to convert the no of years into the actual year in the future

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function compute() {
   var principal = document.getElementById("principal").value;
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
+  var year = new Date().getFullYear() + parseInt(years);
+  var interest = (principal * years * rate) / 100;
 }
